6b50da047a850dd37abb8a8c406b7de34bc558f0
Aashish Bhatt <aashishbhatt258@gmail.com>
2025-05-25 13:20:26 +0545

    add revalidate time

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -15,6 +15,8 @@ import { getArticleById } from "~/server/queries";
 import ArticleContentViewer from "~/components/article-content-viewer";
 import NotFound from "~/app/not-found";
 
+export const revalidate = 60;
+
 export default async function ArticlePage({
   params,
 }: {
